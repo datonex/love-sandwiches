f2eb424b9c540c71430647bd7a68f3fb400a4b94
Tanya G <cgomwe@ymail.com>
2021-08-14 08:22:31 +0100

    Add user requewt loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
@@ -20,27 +20,31 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 # print(data)
 
 def get_sales_data():
-    """
+    '''
     Get sales figures input from the user
-    """
-    
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30.40,50,60\n")
-    
-    data_str = input("Enter your data here:")
-    
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+    '''
+    while True:
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, separated by commas.')
+        print('Example: 10,20,30.40,50,60\n')
+        
+        data_str = input('Enter your data here:')
+        
+        sales_data = data_str.split(',')   
+        
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break
+    return sales_data
 
 def validate_data(values):
-    """
+    '''
     Inside the try, converts all string values into integers.
     Raised ValueError if strings cannot be converted into int, or if there aren't exactly six values
 
     Args:
         values (int): values from sales data
-    """
+    '''
     print(values)
     try:
         [int(value) for value in values]
@@ -48,6 +52,8 @@ def validate_data(values):
             raise ValueError(f'Exactly 6 values required, you provided {len(values)}')
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
+        return False
+    return True
 
-get_sales_data()
+data = get_sales_data()
 # END
