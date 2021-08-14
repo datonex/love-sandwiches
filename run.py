a97be93ca4c9f2f0583b894de7cfe5b9e4bd88bc
Tanya G <cgomwe@ymail.com>
2021-08-14 21:50:03 +0100

    Add stock values data

--- run.py
+++ run.py
@@ -143,9 +143,23 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    return stock_data
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+stock_data = main()
 
+def get_stock_values(data):
+    '''
+    Get stock values and create the recommended stock dictionary
+    '''  
+    
+    headings = SHEET.worksheet('sales').row_values(1)
+    
+    stock_values = dict(zip(headings, data))
+    print(f'Make the following numbers of sandwiches for next market:\n {stock_values} \n')
+    
+    return stock_values   
+
+stock_values = get_stock_values(stock_data)
 
 # END
